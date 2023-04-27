4b0ac8171a6c60fcf8d40ee38db31ccf2da20d28
Tushar Pawar <131463661+Tkpawar@users.noreply.github.com>
2023-04-27 10:42:39 +0530

    Add files via upload

---
+++ Indian Airlines Analysis/Airline_Conclusion and Insight.sql
@@ -0,0 +1,9 @@
+# Conclusion :-
+1)Trujet flight has min price as compared to others flights
+2)while the Jet Airways has higher prices 
+3) Jet Airways consist all over the year has 2535 stops
+
+# Insight 
+1) We need to increase the flight price of the trujet thus it travel a long hour 
+2)Some times Jet Airways flight doeanot have meal for long route travelling
+3) Indigo flight has also not have facilities for long  route travelling
